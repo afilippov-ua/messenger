be0d58076fea2acab8a3d9b6c3ded17d6f1c491d
Alexander Filippov <afilippov.ua@gmail.com>
2015-10-12 21:39:35 +0300

    update sql schema & test data for dao tests

--- src/test/resources/sql/test-data.sql
+++ src/test/resources/sql/test-data.sql
@@ -3,11 +3,11 @@ DELETE FROM MESSAGES;
 DELETE FROM CONTACTS;
 
 INSERT INTO USERS (ID, EMAIL, PASSWORD, NAME, TOKEN) VALUES
-  (1, 'serdyukov@javamonkeys.com', '12345', 'aleksandr serdyukov', '067e6162-3b6f-4ae2-a171-2470b63dff00'),
-  (2, 'sirosh@javamonkeys.com', '12345', 'sergey  sirosh', '54947df8-0e9e-4471-a2f9-9af509fb5889'),
-  (3, 'filippov@javamonkeys.com', '12345', 'aleksandr filippov', ''),
-  (4, 'UpdateUser@javamonkeys.com', '12345', 'update user', ''),
-  (5, 'DeleteUser@javamonkeys.com', '12345', 'delete user', '');
+  (1, 'serdyukov@mail.com', '12345', 'aleksandr serdyukov'),
+  (2, 'sirosh@mail.com', '12345', 'sergey  sirosh'),
+  (3, 'filippov@mail.com', '12345', 'aleksandr filippov'),
+  (4, 'UpdateUser@mail.com', '12345', 'update user'),
+  (5, 'DeleteUser@mail.com', '12345', 'delete user');
 
 INSERT INTO MESSAGES (ID, MESSAGE_DATE, SENDER_ID, RECEIVER_ID, TEXT, SEEN) VALUES
   (1, PARSEDATETIME('2015/01/01 00:00:00', 'yyyy/MM/dd hh:mm:ss'), 3, 1, 'Hi!', TRUE),
