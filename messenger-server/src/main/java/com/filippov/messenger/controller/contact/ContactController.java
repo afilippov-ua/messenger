807f365092784dfac0c492281eaed6b16e5ef851
alexander filippov <afilippov.ua@gmail.com>
2015-11-25 18:35:20 +0200

    update contact controller

--- messenger-server/src/main/java/com/filippov/messenger/controller/contact/ContactController.java
+++ messenger-server/src/main/java/com/filippov/messenger/controller/contact/ContactController.java
@@ -19,12 +19,10 @@ public class ContactController implements IContactController {
     IContactService contactService;
 
     @Transactional
-    @RequestMapping(
-            value = "/{ownerId}/contacts/{contactId}",
-            method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity createContact(
-            @PathVariable("ownerId") Integer ownerId,
-            @PathVariable("contactId") Integer contactId,
+            @RequestParam("ownerId") Integer ownerId,
+            @RequestParam("contactId") Integer contactId,
             @RequestParam("name") String name) {
         if (contactService.createContact(ownerId, contactId, name) == null)
             return new ResponseEntity(HttpStatus.NOT_FOUND);
@@ -34,7 +32,7 @@ public class ContactController implements IContactController {
 
     @Transactional
     @RequestMapping(
-            value = "/{userId}/contacts/{id}",
+            value = "/{id}",
             method = RequestMethod.GET)
     public ResponseEntity<Contact> getContact(@PathVariable("id") Integer id) {
         Contact contact = contactService.getContact(id);
@@ -46,11 +44,9 @@ public class ContactController implements IContactController {
     }
 
     @Transactional
-    @RequestMapping(
-            value = "/{userId}/contacts",
-            method = RequestMethod.GET)
-    public ResponseEntity<List<Contact>> getContacts(@PathVariable("userId") Integer userId) {
-        return new ResponseEntity<>(contactService.getContacts(userId), HttpStatus.OK);
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<Contact>> getContacts(@RequestParam("ownerId") Integer ownerId) {
+        return new ResponseEntity<>(contactService.getContacts(ownerId), HttpStatus.OK);
     }
 
     @RequestMapping(
@@ -67,7 +63,7 @@ public class ContactController implements IContactController {
 
     @Transactional
     @RequestMapping(
-            value = "/{ownerId}/contacts/{id}",
+            value = "/{id}",
             method = RequestMethod.DELETE)
     public ResponseEntity deleteContact(@PathVariable("id") Integer id) {
         if (contactService.deleteContact(id))
